f2c15cc30d18ccdb3bd234f3814c4122b4bc02d4
PenguinAlex <besedinalexey@gmail.com>
2019-02-19 17:41:36 +0300

    Lab_05

--- Lab_05/ts/script.ts
+++ Lab_05/ts/script.ts
@@ -1,7 +1,13 @@
-let date = new Date()
-console.log(date)
+let data: HTMLBaseElement
+window.onload = function() {
+    data = document.querySelector("#time")
+}
+setInterval(() => {
+    let date = new Date()
+    data.innerHTML = date.getDay() + ":" + date.getMonth() + ":" + date.getFullYear() + ":" + date.getHours() + ":" + date.getMinutes() + ":" +date.getSeconds() + ":" +date.getMilliseconds()
+}, 1)
 
-setTimeout(function() { console.log("Hey") }, 1000)
+setTimeout(() => { console.log("Hey") }, 1000)
 
 // let obj = {}
 // obj.name = "Name"
