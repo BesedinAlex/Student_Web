91bd07d78271bd8934cffec69f56bfba9d434f29
PenguinAlex <besedinalexey@gmail.com>
2019-03-05 19:55:30 +0300

    Game changes
    - Added a menu to start or stop the game.
    - Removed checkpoints.
    - Auto-clicker has no use anymore.

--- Lab_06/src/script.ts
+++ Lab_06/src/script.ts
@@ -1,17 +1,7 @@
-let count: number, randomNumber: number = 0;
+let count = 0, randomNumber = 0;
+let playable = false, catched = false;
 let balls: HTMLCollectionOf<HTMLBaseElement>;
 
-// Init game
-window.onload = function() {
-    count = 0;
-    balls = document.getElementsByClassName("ball") as HTMLCollectionOf<HTMLBaseElement>;
-    for (let i = 1; i < 10; i++) {
-        (document.querySelector("#r" + i) as HTMLElement).onclick = () => match(i - 1, 1);
-        (document.querySelector("#b" + i) as HTMLElement).onclick = () => match(i - 1, 0);
-    }
-}
-
-// Game functions
 function activateBall(index: number) {
     balls[index].style.background = "#ff0000";
 }
@@ -19,30 +9,46 @@ function deactivateBall(index: number) {
     balls[index].style.background = "#ffffff";
 }
 function match(index: number, type: number) {
-    if (type === 0) {
-        if (index === randomNumber) {
-            count += 2;
-            switch (count) {
-                case 21:
-                    alert("Checkpoint!\nYou already got 20 points.\nDo you wish to continue?\nNext checkpoint is 50 points.");
-                    break;
-                case 51:
-                    alert("Checkpoint!\nYou got 50 points.\nDo you wish to continue?\nThe last checkpoint is 100 points.\nYou get a cool movie if you get there.");
-                    break;
-                case 101:
-                    alert("The last checkpoint!\nYou rock!\nClose this window to see the movie.");
-                    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-                    break;
-            }
-        }
-    } else
-        count--;
-    document.querySelector("#count").innerHTML = String(count);
+    if (playable) {
+        if (type === 0) {
+            if (index === randomNumber)
+                if (!catched) {
+                    count += 2;
+                    catched = true;
+                } else
+                    count++;
+        } else
+            count--;
+        document.querySelector("#count").innerHTML = String(count);
+    }
+}
+function gameLoop() {
+    deactivateBall(randomNumber);
+    randomNumber = Math.floor(Math.random() * 9);
+    activateBall(randomNumber);
+    catched = false;
 }
 
-// Infinite loop
-setInterval(() => {
-    deactivateBall(randomNumber);
-    randomNumber = Math.floor(Math.random() * (9 - 0));
-    activateBall(randomNumber);
-}, 1000)
+window.onload = () => {
+    balls = document.getElementsByClassName("ball") as HTMLCollectionOf<HTMLBaseElement>;
+    for (let i = 1; i < 10; i++) {
+        document.querySelector("#r" + i).addEventListener("click", () => match(i - 1, 1));
+        document.querySelector("#b" + i).addEventListener("click", () => match(i - 1, 0));
+    }
+    document.getElementById("playground").oncontextmenu = (event) => {
+        document.getElementById("menu").style.left = String(event.clientX);
+        document.getElementById("menu").style.top = String(event.clientY);
+        return false;
+    }
+    let game: any;
+    document.querySelector("body").addEventListener("click", (event) => document.getElementById("menu").style.left = document.getElementById("menu").style.top = String(-500));
+    document.getElementById("menu-1").addEventListener("click", (event) => {
+        playable = true;
+        game = setInterval(gameLoop, 1000);
+    });
+    document.getElementById("menu-2").addEventListener("click", (event) => {
+        clearInterval(game);
+        deactivateBall(randomNumber);
+        playable = false;
+    });
+}
