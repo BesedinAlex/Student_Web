a0a8814de7f7bfa689e64f825240cf19ed275e15
Alexey <besedinalexey@gmail.com>
2019-04-23 21:44:57 +0300

    Visual updates

--- Lab18/src/app/app-routing.module.ts
+++ Lab18/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {MainPageComponent} from './main-page/main-page.component';
+
+const routes: Routes = [
+  {path: '', component: MainPageComponent}
+];
 
 @NgModule({
-  declarations: [],
   imports: [
+    RouterModule.forRoot(routes),
     CommonModule
-  ]
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
